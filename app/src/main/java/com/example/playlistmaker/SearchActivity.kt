338c4e936cf5cd838a3c0322db084037d478e872
Marat Ikhsanov <ihsmen@gmail.com>
2024-08-18 07:57:03 +0200

    Сделать экран поиска

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,20 +1,87 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+    private var searchInput: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val arrowBack = findViewById<ImageView>(R.id.arrow_back)
+        arrowBack.setOnClickListener {
+            val mainDisplay = Intent(this, MainActivity::class.java)
+            startActivity(mainDisplay)
+        }
+
+        val linearLayout = findViewById<FrameLayout>(R.id.container)
+        val inputEditText = findViewById<EditText>(R.id.textSearch)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        if(savedInstanceState != null) {
+            inputEditText.setText(INPUT_SEARCH)
+        }
+
+        fun clearButtonVisibility(inputString: CharSequence?): Int {
+            return if(inputString.isNullOrEmpty()) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // empty
+                searchInput = s.toString()
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_SEARCH, searchInput)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchInput = savedInstanceState.getString(INPUT_SEARCH, "")
+    }
+    companion object {
+        private const val INPUT_SEARCH = "INPUT_SEARCH"
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -16,10 +17,36 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val arrowBack = findViewById<ImageView>(R.id.arrow_back)
-
         arrowBack.setOnClickListener {
             val mainDisplay = Intent(this, MainActivity::class.java)
             startActivity(mainDisplay)
+        }
+
+        val shareButton = findViewById<ImageView>(R.id.share)
+        shareButton.setOnClickListener {
+            val messageIntent = Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_TEXT, R.string.share_link)
+                type = "text/plain"
+                Intent.createChooser(this, null)
+            }
+            startActivity(messageIntent)
+        }
+
+        val support = findViewById<ImageView>(R.id.support)
+        support.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, R.string.support_email)
+                putExtra(Intent.EXTRA_SUBJECT, R.string.support_subject)
+                putExtra(Intent.EXTRA_TEXT, R.string.support_text)
+            }
+            startActivity(supportIntent)
+        }
+
+        val agreement = findViewById<ImageView>(R.id.agreement)
+        agreement.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.agreement_link)))
+            startActivity(agreementIntent)
         }
     }
 }
