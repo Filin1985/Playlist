280d82444bb732dacddcaf1784f03ca4dd7ec4e8
Marat Ikhsanov <ihsmen@gmail.com>
2025-04-13 14:18:39 +0300

    Make image changable by click

--- app/src/main/java/com/example/playlistmaker/ui/player/customView/PlaybackButtonView.kt
+++ app/src/main/java/com/example/playlistmaker/ui/player/customView/PlaybackButtonView.kt
@@ -5,11 +5,13 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
 import androidx.core.graphics.drawable.toBitmap
 import com.example.playlistmaker.R
+import com.example.playlistmaker.domain.player.model.PlayButtonState
 
 class PlaybackButtonView @JvmOverloads constructor(
     context: Context,
@@ -18,7 +20,9 @@ class PlaybackButtonView @JvmOverloads constructor(
     @StyleRes defStyleRes: Int = 0,
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
 
-    private val imageBitmap: Bitmap?
+    private val playImageBitmap: Bitmap?
+    private val pauseImageBitmap: Bitmap?
+    private var playButtonState = PlayButtonState.STATE_PLAY
     private var imageRect = RectF(0f, 0f, 0f, 0f)
 
     init {
@@ -29,7 +33,10 @@ class PlaybackButtonView @JvmOverloads constructor(
             defStyleRes
         ).apply {
             try {
-                imageBitmap = getDrawable(R.styleable.PlaybackButtonView_imageResId)?.toBitmap()
+                playImageBitmap =
+                    getDrawable(R.styleable.PlaybackButtonView_playImageResId)?.toBitmap()
+                pauseImageBitmap =
+                    getDrawable(R.styleable.PlaybackButtonView_pauseImageResId)?.toBitmap()
             } finally {
                 recycle()
             }
@@ -42,8 +49,34 @@ class PlaybackButtonView @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas) {
-        imageBitmap?.let {
-            canvas.drawBitmap(imageBitmap, null, imageRect, null)
+        when (playButtonState) {
+            PlayButtonState.STATE_PLAY -> playImageBitmap?.let {
+                canvas.drawBitmap(playImageBitmap, null, imageRect, null)
+            }
+
+            PlayButtonState.STATE_PAUSE -> pauseImageBitmap?.let {
+                canvas.drawBitmap(pauseImageBitmap, null, imageRect, null)
+            }
+
         }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                return true
+            }
+
+            MotionEvent.ACTION_UP -> {
+                playButtonState = when (playButtonState) {
+                    PlayButtonState.STATE_PLAY -> PlayButtonState.STATE_PAUSE
+                    PlayButtonState.STATE_PAUSE -> PlayButtonState.STATE_PLAY
+                }
+                invalidate()
+                performClick()
+                return true
+            }
+        }
+        return super.onTouchEvent(event)
     }
 }
