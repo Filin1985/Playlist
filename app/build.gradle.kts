8491ad4de0a26d36f2a7a25d31348d20520bc191
Marat Ikhsanov <ihsmen@gmail.com>
2025-05-18 12:33:31 +0300

    Подключить необходимые библиотеки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,12 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.9"
+    }
 }
 
 dependencies {
@@ -86,4 +92,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
